77d3952a647d27aecb0c63f2250617b70387d6ed
zhudiana <yoditfentaw90@gmail.com>
2025-10-09 22:10:01 +0200

    local db changed to production db

--- app/db/database.py
+++ app/db/database.py
@@ -6,8 +6,7 @@ from app.core.config import DATABASE_URL
 
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={'check_same_thread': False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
